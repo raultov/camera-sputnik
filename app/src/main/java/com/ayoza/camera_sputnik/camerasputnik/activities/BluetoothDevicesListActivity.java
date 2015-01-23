c9dd53dd67c33ccded5cac492bdfeee7cbe5b3de
Raúl Tovar <angel.raul.tovar.martin@gmail.com>
2015-01-23 18:33:01 +0100

    Added BluetoothDevicesListActivity

---
+++ app/src/main/java/com/ayoza/camera_sputnik/camerasputnik/activities/BluetoothDevicesListActivity.java
@@ -0,0 +1,13 @@
+package com.ayoza.camera_sputnik.camerasputnik.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by raul on 23/01/15.
+ */
+public class BluetoothDevicesListActivity extends Activity {
+    
+    
+    
+    
+}
