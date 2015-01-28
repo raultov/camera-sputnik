879891b5797147e8783abc294d310c7d770ac8fe
Raúl Tovar <angel.raul.tovar.martin@gmail.com>
2015-01-28 22:42:52 +0100

    ConfigurationMgr added

---
+++ app/src/main/java/com/ayoza/camera_sputnik/camerasputnik/arduino/managers/ConfigurationMgr.java
@@ -0,0 +1,18 @@
+package com.ayoza.camera_sputnik.camerasputnik.arduino.managers;
+
+/**
+ * Created by raul on 28/01/15.
+ */
+/**
+ * This class is responsible for creating and managing settings
+ *
+ */
+public class ConfigurationMgr {
+    
+    private static ConfigurationMgr instance = null;
+    
+    private ConfigurationMgr() {
+        
+        
+    }
+}
