42ede46e865ee5b6862c630e5a32ca24cc4529d2
Raúl Tovar <angel.raul.tovar.martin@gmail.com>
2015-02-11 22:52:51 +0100

    Activity moved

---
+++ app/src/main/java/com/ayoza/camera_sputnik/camerasputnik/views/ItemDeviceBluetooth.java
@@ -0,0 +1,16 @@
+package com.ayoza.camera_sputnik.camerasputnik.views;
+
+import android.content.Context;
+import android.widget.TextView;
+
+/**
+ * Created by raul on 11/02/15.
+ */
+public class ItemDeviceBluetooth extends TextView {
+    
+    public ItemDeviceBluetooth(Context context) {
+        super(context);
+    }
+    
+    
+}
