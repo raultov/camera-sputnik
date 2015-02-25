2452bf59e6b2e9cf48c34fee3c094ca1ac9001d1
Raúl Tovar <angel.raul.tovar.martin@gmail.com>
2015-02-25 22:22:32 +0100

    Added handler to communicate with main thread

---
+++ app/src/main/java/com/ayoza/camera_sputnik/camerasputnik/interfaces/OnBackgroundListener.java
@@ -0,0 +1,9 @@
+package com.ayoza.camera_sputnik.camerasputnik.interfaces;
+
+/**
+ * Created by raul on 23/02/15.
+ */
+public interface OnBackgroundListener {
+    
+    public void onBackground();
+}
